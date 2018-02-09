57e522dcd79a2f9915ee1df9d515eab2df4cd7d9
Unknown <Bomber.qw@gmail.com>
2018-02-09 19:15:04 +0200

    Add car form

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,5 @@
 class ServicesController < ApplicationController
+  def new
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'home/index'
   resources :services
   root 'home#index'
+  get 'services' => 'pages#services'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
