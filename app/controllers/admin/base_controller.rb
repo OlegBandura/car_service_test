29025561edd65ebe01a92cf04edebd51f480e299
Unknown <Bomber.qw@gmail.com>
2018-02-19 21:52:04 +0200

    Create admin/user role

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,10 @@
 class Admin::BaseController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
+  before_action :check_admin
+  layout "admin"
+  protected
+  def check_admin
+    redirect_to root_path, alert: "Ви не маєте прав доступу до цієї сторінки" unless current_user.admin?
+  end
 
 end
